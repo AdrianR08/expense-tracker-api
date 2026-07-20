4ef1cd06c8bda5f522c759103a15eedfca83743f
AdrianR08 <159833076+AdrianR08@users.noreply.github.com>
2026-07-20 12:12:00 -0400

    added front end details such as recent expenses card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,21 +17,36 @@ function App() {
       <h1>Expense Tracker</h1>
       <p>Track and understand your spending.</p>
 
-      <section>
-        <h2>Expenses</h2>
+      <section className="expense-card">
+
+        <div className="expense-card-header">
+          <h2 className="expense-card-title">Recent Expenses</h2>
+        </div>
         <ul>
           {expenses.map((expense) => (
             <li className="expense-item" key={expense.id}>
               <div className="expense-details">
                 <div className="expense-merchant">{expense.merchant}</div>
+                 <div className="expense-info">
                 <div className="expense-category">{expense.category}</div>
-                <div className="expense-date">{expense.expense_date}</div>
+                <p>•</p>
+                <div className="expense-date">{new Date(`${expense.expense_date}T00:00:00`).toLocaleDateString(
+                  "en-US",
+                  {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                  }
+                )}</div>
+                </div>
               </div>
               <div className="expense-amount">${expense.amount.toFixed(2)}</div>
               </li>
           ))}
 
         </ul>
+
+        <button className="expense-card-button">View All</button>
       </section>
 
     </main>
