73f4ea4dc309af9004fcd954ce3576b84807113a
AdrianR08 <159833076+AdrianR08@users.noreply.github.com>
2026-07-26 13:12:12 -0400

    added the nav, and add transaction form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
 import './App.css'
 import { useEffect, useState } from 'react'
+import { House, Coins, BookCopy , Scroll, Settings} from 'lucide-react';
+import {
+  PieChart,
+  Pie,
+  ResponsiveContainer,
+  Tooltip
+} from 'recharts'
 
 
 function App() {
@@ -38,8 +45,6 @@ function App() {
     const createdExpenses = await response.json()
 
     setExpenses([createdExpenses, ...expenses])
-
-    console.log(jsonResponse)
   }
 
   useEffect(() => {
@@ -52,110 +57,135 @@ function App() {
 
   return (
     <main>
-      <h1>Expense Tracker</h1>
-      <p>Track and understand your spending.</p>
+      <div className='background'>
+        <div className='first-column'>
 
-      <div className='expenses-lists'>
-        <section className="expense-card">
-          <div className="expense-card-header">
-            <h2>Recent Expenses</h2>
+          <button className='button-nav'> <House className='nav-icon'/>
+            Dashboard</button>
+
+          <button className='button-nav'> <Coins className='nav-icon' />Expenses</button>
+
+          <button className='button-nav'><BookCopy className='nav-icon' />Categories</button>
+
+          <button className='button-nav'><Scroll className='nav-icon' />Summary</button>
+
+          <button className='button-nav'><Settings />Settings</button>
+
+        </div>
+
+        <div className='second-column'>
+          <div className='header'>
+            <h1>Expenses</h1>
+            <p>Track and understand your spending.</p>
           </div>
 
-          <div className='list-items'>
-            <ul>
-              {[...expenses].sort((a, b) => new Date(b.expense_date) - new Date(a.expense_date)).slice(0, 4).map((expense) => (
-                <li className="expense-item" key={expense.id}>
-                  <div className="expense-details">
-                    <div className="expense-merchant">{expense.merchant}</div>
-                    <div className="expense-info">
-                      <div className="expense-category">{expense.category}</div>
-                      <p>•</p>
-                      <div className="expense-date">{new Date(`${expense.expense_date}T00:00:00`).toLocaleDateString(
-                        "en-US",
-                        {
-                          month: "long",
-                          day: "numeric",
-                          year: "numeric",
-                        }
-                      )}
+          <section className='overview'>
+            
+
+          </section>
+          <div className='expenses-lists'>
+            <section className="expense-card">
+              <div className="expense-card-header">
+                <h2>Recent Expenses</h2>
+              </div>
+
+              <div className='list-items'>
+                <ul>
+                  {[...expenses].sort((a, b) => new Date(b.expense_date) - new Date(a.expense_date)).slice(0, 4).map((expense) => (
+                    <li className="expense-item" key={expense.id}>
+                      <div className="expense-details">
+                        <div className="expense-merchant">{expense.merchant}</div>
+                        <div className="expense-info">
+                          <div className="expense-category">{expense.category}</div>
+                          <p>•</p>
+                          <div className="expense-date">{new Date(`${expense.expense_date}T00:00:00`).toLocaleDateString(
+                            "en-US",
+                            {
+                              month: "long",
+                              day: "numeric",
+                              year: "numeric",
+                            }
+                          )}
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className="expense-amount">
-                    ${expense.amount.toFixed(2)}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <button className="expense-card-button">View All Expenses</button>
+                      <div className="expense-amount">
+                        ${expense.amount.toFixed(2)}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <button className="expense-card-button">View All Expenses</button>
 
-        </section>
+            </section>
 
-        <section className='add-expense-card'>
-          <div className='expense-card-header'>
-            <h2>Add Expense</h2>
-          </div>
-          <form className='form-style' onSubmit={handleSubmit}>
+            <section className='add-expense-card'>
+              <div className='expense-card-header'>
+                <h2>Add Expense</h2>
+              </div>
+              <form className='form-style' onSubmit={handleSubmit}>
 
-            <div className='add-merchant-item'>
+                <div className='add-merchant-item'>
 
-              <h2 className='sub-header'>Merchant *</h2>
+                  <h2 className='sub-header'>Merchant *</h2>
 
-              <input type='text' className='input-merchant' placeholder='eg. Starbucks' required value={merchant}
-                onChange={(e) => setMerchant(e.target.value)}></input>
-            </div>
-
-            <div>
-
-              <h2 className='sub-header'>Category *</h2>
-
-              <select className='select-menu' required value={category} onChange={(e) => setCategory(e.target.value)}>
-                <option value='' disabled>
-                  Select a category
-                </option>
-                <option className='menu-options'>Housing</option>
-                <option className='menu-options'>Transportation</option>
-                <option className='menu-options'>Food</option>
-                <option className='menu-options'>Utilities</option>
-                <option className='menu-options'>Clothing</option>
-                <option className='menu-options'>Medical/Healthcare</option>
-                <option className='menu-options'>Insurance</option>
-                <option className='menu-options'>Household Items/Supplies</option>
-                <option className='menu-options'>Personal</option>
-                <option className='menu-options'>Debt</option>
-                <option className='menu-options'>Retirement</option>
-                <option className='menu-options'>Education</option>
-                <option className='menu-options'>Savings</option>
-                <option className='menu-options'>Gifts/Donations</option>
-                <option className='menu-options'>Entertainment</option>
-                <option className='menu-options'>Other</option>
-              </select>
-            </div>
-
-            <div>
-              <h2 className='sub-header'>Amount *</h2>
-              <div className='add-amount-item'>
-                <div className='text-symbol'>
-                  <p className='dollar-symbol'>$</p>
+                  <input type='text' className='input-merchant' placeholder='eg. Starbucks' required value={merchant}
+                    onChange={(e) => setMerchant(e.target.value)}></input>
                 </div>
-                <input className='input-amount' placeholder='0.00' type='number' required value={amount}
-                  onChange={(e) => setAmount(e.target.value)}></input>
-              </div>
-            </div>
 
-            <div>
-              <h2 className='sub-header'>Date *</h2>
-              <div className='add-date-item'>
-                <input className='date-input' type='date' value={expense_date} required onChange={(e) => setDate(e.target.value)}>
-                </input>
-              </div>
-            </div>
+                <div>
 
-            <button className='add-expense-button' value='Submit' >Add Expense</button>
+                  <h2 className='sub-header'>Category *</h2>
 
-          </form>
-        </section>
+                  <select className='select-menu' required value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value='' disabled>
+                      Select a category
+                    </option>
+                    <option className='menu-options'>Housing</option>
+                    <option className='menu-options'>Transportation</option>
+                    <option className='menu-options'>Food</option>
+                    <option className='menu-options'>Utilities</option>
+                    <option className='menu-options'>Clothing</option>
+                    <option className='menu-options'>Medical/Healthcare</option>
+                    <option className='menu-options'>Insurance</option>
+                    <option className='menu-options'>Household Items/Supplies</option>
+                    <option className='menu-options'>Personal</option>
+                    <option className='menu-options'>Debt</option>
+                    <option className='menu-options'>Retirement</option>
+                    <option className='menu-options'>Education</option>
+                    <option className='menu-options'>Savings</option>
+                    <option className='menu-options'>Gifts/Donations</option>
+                    <option className='menu-options'>Entertainment</option>
+                    <option className='menu-options'>Other</option>
+                  </select>
+                </div>
+
+                <div>
+                  <h2 className='sub-header'>Amount *</h2>
+                  <div className='add-amount-item'>
+                    <div className='text-symbol'>
+                      <p className='dollar-symbol'>$</p>
+                    </div>
+                    <input className='input-amount' placeholder='0.00' type='number' required value={amount}
+                      onChange={(e) => setAmount(e.target.value)}></input>
+                  </div>
+                </div>
+
+                <div>
+                  <h2 className='sub-header'>Date *</h2>
+                  <div className='add-date-item'>
+                    <input className='date-input' type='date' value={expense_date} required onChange={(e) => setDate(e.target.value)}>
+                    </input>
+                  </div>
+                </div>
+
+                <button className='add-expense-button' value='Submit' >Add Expense</button>
+
+              </form>
+            </section>
+          </div>
+        </div>
       </div>
     </main >
   )
